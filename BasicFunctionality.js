aef9cae99e11b5e80adf4f933f034b06508a9dd7
CarlBinneman <carlbbinneman@gmail.com>
2020-03-12 12:41:54 +0200

    Initial Commit
    Committing a basic website with swapping image and links

---
+++ BasicFunctionality.js
@@ -0,0 +1,37 @@
+let myImage = document.querySelector('img');
+let myParagraph = document.querySelector('p');
+let myLink = document.querySelector('a');
+let switchCheck = 0;
+
+ShowSet_0();
+
+myImage.onclick = function() 
+{
+    if(switchCheck === 0) 
+    {
+        switchCheck = 1;     
+        ShowSet_1();
+    } 
+    else 
+    {
+        switchCheck = 0;        
+        ShowSet_0();
+    }
+    //console.log(switchCheck);
+}
+
+function ShowSet_0 () 
+{
+    myImage.setAttribute ('src','Images/website-drawing-scan.png');
+    myParagraph.textContent = "The above is a hand rendering from : ";
+    myLink.textContent = "The Mozilla Website";
+    myLink.setAttribute('href', 'https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/What_will_your_website_look_like');        
+}
+
+function ShowSet_1 () 
+{
+    myImage.setAttribute ('src','Images/example-red-square-grunge-stamp-260nw-327662909.jpg');
+    myParagraph.textContent = "The above is a stock image from : ";
+    myLink.textContent = "Shutterstock";
+    myLink.setAttribute('href', 'https://www.shutterstock.com/search/example+stamp');        
+}
